64267a2326d28c9ed4a15b014f2a12bcd64c980d
Tanndlin <tanndlin@gmail.com>
2022-10-22 14:50:27 -0400

    Set up calendar

---
+++ web/frontend/src/common/Event.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+interface IEvent {
+    title: string;
+    start: Date;
+    end: Date;
+    allDay?: boolean;
+    resource?: any;
+}
--- web/frontend/src/components/Calendar.js
+++ web/frontend/src/components/Calendar.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
 
-function Calendar() {
-    return (
-        <header className='h-1/10 bg-red-500'>
-            <h1 className='py-3 center text-center text-4xl font-bold'>Bill Tracker</h1>
-        </header>
-    );
-};
-export default Calendar;
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import '../App.css';
+
+const localizer = momentLocalizer(moment)
+
+const myEventsList = [
+    {
+        title: 'Weekend',
+        start: new Date(),
+        end: new Date(moment().add(1, 'days'))
+    }
+];
+
+function handleCalendarClick(e) {
+    e.preventDefault();
+    if (e.target.classList.contains('rbc-event-content')) {
+        console.log('Event Clicked');
+
+        // Open modal
+
+    }
+}
+
+const BigCalendar = (props) => (
+    <main className='flex bg-orange-200 h-9/10'>
+        <div className='grid container m-auto h-4/5 bg-yellow-200 flex-1'>
+            <span onClick={handleCalendarClick}>
+                <Calendar
+                    localizer={localizer}
+                    events={myEventsList}
+                    startAccessor="start"
+                    endAccessor="end"
+                    style={{ height: 500 }}
+                />
+            </span>
+        </div>
+    </main>
+)
+export default BigCalendar;

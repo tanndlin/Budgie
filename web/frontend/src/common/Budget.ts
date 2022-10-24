6053ca6a6701a99d059e9ef94cd6ecc8e8e2513c
tanndlin <tanndlin@gmail.com>
2022-10-24 18:18:37 -0400

    Dynamically add Budget components

---
+++ web/frontend/src/common/Budget.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+interface IBudget{
+    id: string;
+    name: string;
+    total: number;
+    spent: number;
+    payments: number[];
+}
--- web/frontend/src/components/BudgetsView.js
+++ web/frontend/src/components/BudgetsView.js
@@ -5,24 +5,41 @@ import 'react-circular-progressbar/dist/styles.css';
 
 // TOOD: Change color of progress bar based on percentage
 // TODO: Add ability to add/sub from spent budget
-// TODO: Dynamically add progress bar components based on number of budgets
 function BudgetsView() {
+
+    const [budgets, setBudgets] = React.useState([
+        {
+            name: "Groceries",
+            spent: 100,
+            total: 200,
+        },
+        {
+            name: "Gas",
+            spent: 80,
+            total: 100,
+        },
+    ]);
+
     return (
         <article className='m-auto container bg-yellow-200 p-3 mb-5 rounded-md'>
             <h1 className='text-2xl font-bold border-black border-b-2 p-1'>Budgets</h1>
             <section className='flex flex-cols gap-x-16 p-16'>
-                <div className='grid grid-cols-1'>
-                    <h2 className='text-xl font-bold'>Groceries</h2>
-                    <span style={{ width: 200, height: 200 }}>
-                        <CircularProgressbar value={66} text={`${66}%`} />
-                    </span>
-                </div>
-                <div className='grid grid-cols-1'>
-                    <h2 className='text-xl font-bold'>Eating out</h2>
-                    <span style={{ width: 200, height: 200 }}>
-                        <CircularProgressbar value={75} text={`${75}%`} />
-                    </span>
-                </div>
+
+                {
+                    budgets.map((budget) => {
+                        const percent = (budget.spent / budget.total) * 100;
+
+                        return (
+                            <div className='grid grid-cols-1'>
+                                <h2 className='text-xl font-bold'>{budget.name}</h2>
+                                <span style={{ width: 200, height: 200 }}>
+                                    <CircularProgressbar value={percent} text={`${percent}%`} />
+                                </span>
+                            </div>
+                        );
+                    })
+                }
+
             </section>
         </article>
     );

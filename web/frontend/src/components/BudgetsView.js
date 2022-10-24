ef777bba1086196c45a4cb251fb5ebd9c8e1c150
tanndlin <tanndlin@gmail.com>
2022-10-24 18:26:48 -0400

    Change Color based on percent spent

--- web/frontend/src/components/BudgetsView.js
+++ web/frontend/src/components/BudgetsView.js
@@ -1,13 +1,31 @@
+/* eslint-disable no-mixed-operators */
 import React from 'react';
-import { CircularProgressbar } from 'react-circular-progressbar';
+import { CircularProgressbar,buildStyles  } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
 
-// TOOD: Change color of progress bar based on percentage
 // TODO: Add ability to add/sub from spent budget
 function BudgetsView() {
+    function lerpColor(a, b, amount) { 
+
+        var ah = parseInt(a.replace(/#/g, ''), 16),
+            ar = ah >> 16, ag = ah >> 8 & 0xff, ab = ah & 0xff,
+            bh = parseInt(b.replace(/#/g, ''), 16),
+            br = bh >> 16, bg = bh >> 8 & 0xff, bb = bh & 0xff,
+            rr = ar + amount * (br - ar),
+            rg = ag + amount * (bg - ag),
+            rb = ab + amount * (bb - ab);
+
+        return '#' + ((1 << 24) + (rr << 16) + (rg << 8) + rb | 0).toString(16).slice(1);
+}
+
 
     const [budgets, setBudgets] = React.useState([
+        {
+            name: "Clothes",
+            spent: 10,
+            total: 200,
+        },
         {
             name: "Groceries",
             spent: 100,
@@ -28,12 +46,19 @@ function BudgetsView() {
                 {
                     budgets.map((budget) => {
                         const percent = (budget.spent / budget.total) * 100;
+                        
+                        // Lerp color from green to red
+                        const red = '#f74f31';
+                        const green = '#7aff75';
+                        const color = lerpColor(green, red, percent / 100);
+
+                        console.log(color);
 
                         return (
                             <div className='grid grid-cols-1'>
                                 <h2 className='text-xl font-bold'>{budget.name}</h2>
                                 <span style={{ width: 200, height: 200 }}>
-                                    <CircularProgressbar value={percent} text={`${percent}%`} />
+                                    <CircularProgressbar styles={buildStyles({pathColor:color})} value={percent} text={`${percent}%`} />
                                 </span>
                             </div>
                         );

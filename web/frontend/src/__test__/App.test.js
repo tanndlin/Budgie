1c0736af12588ce558bcd12558893aa13ae821f5
Tanner Sandlin <tanndlin@gmail.com>
2022-10-27 11:08:45 -0400

    Test home page

--- web/frontend/src/__test__/App.test.js
+++ web/frontend/src/__test__/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders learn react link', () => {
+test('renders header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Bill Tracker/i);
-  expect(linkElement).toBeInTheDocument();
+  const header = screen.getByText(/Bill Tracker/i);
+  expect(header).toBeInTheDocument();
+});
+
+test('renders login page', () => {
+  render(<App />);
+  const h1 = screen.getByText(/Please Log In/i);
+  expect(h1).toBeInTheDocument();
 });

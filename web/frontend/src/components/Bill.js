feab961f18783f1760d011ac89ab2e533f17bdae
Tanndlin <tanndlin@gmail.com>
2022-10-27 13:28:29 -0400

    Change paid colors

--- web/frontend/src/components/Bill.js
+++ web/frontend/src/components/Bill.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 function Bill(props) {
+    const className = 'p-2 rounded-md' + (props.bill.resources.paid ? ' paid' : ' unpaid');
+
     return (
-        <div className="bg-white p-3 rounded-md">
+        <div className={className}>
             <h1 className="text-2xl font-bold">{props.bill.title}</h1>
             <p className="text-xl font-bold">${props.bill.amount}</p>
             <p className="text-xl font-bold">{props.bill.start.toISOString()}</p>
--- web/frontend/src/components/ListView.js
+++ web/frontend/src/components/ListView.js
@@ -2,13 +2,11 @@ import React from 'react';
 import Bill from './Bill';
 
 function ListView(props) {
-    console.log('Bills: ', props.bills);
-
     return (
         <div className="flex h-full">
             <article className="container m-auto min-h-500 bg-yellow-200 p-3 rounded-md">
                 <h1 className="text-2xl font-bold border-black border-b-2 p-1">List</h1>
-                <section id="listView" className="grid gap-16 justify-center p-16">
+                <section id="listView" className="grid gap-16 p-16">
                     {props.bills.map((bill) => (
                         <Bill bill={bill} />
                     ))}

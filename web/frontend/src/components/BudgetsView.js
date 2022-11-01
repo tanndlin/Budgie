6e07fe451114b33d6286d5f314be7694115e21d4
Tanner Sandlin <tanndlin@gmail.com>
2022-11-01 11:31:09 -0400

    Sum for Budgets

--- web/frontend/src/components/BudgetsView.js
+++ web/frontend/src/components/BudgetsView.js
@@ -4,7 +4,23 @@ import Budget from './Budget';
 function BudgetsView(props) {
     return (
         <article className="m-auto container bg-yellow-200 p-3 mb-5 rounded-md">
-            <h1 className="text-2xl font-bold border-black border-b-2 p-1">Budgets</h1>
+            <div className="flex flex-row justify-between font-bold border-black border-b-2 p-1">
+                <h1 className="text-2xl">Budgets</h1>
+                <span className='text-md'>
+                    <h2 className=''>{
+                        `Total:  $${Object.entries(props.budgets)
+                            .reduce((acc, [key, budget]) =>
+                                acc + Number(budget.total), 0)}
+                        / month`
+                    }</h2>
+                    <h2 className=''>{
+                        `Spent: $${Object.entries(props.budgets)
+                            .reduce((acc, [key, budget]) =>
+                                acc + Number(budget.spent), 0)}
+                        / month`
+                    }</h2>
+                </span>
+            </div>
             <section id="budgetView" className="grid gap-16 justify-center p-16">
                 {props.budgets.map((budget) => (
                     <Budget

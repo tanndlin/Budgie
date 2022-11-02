dc78d72a69326e44bf69ce0d56f0104b3fbba81f
Tanndlin <tanndlin@gmail.com>
2022-11-02 18:26:17 -0400

    Login test

--- web/frontend/src/components/Login.js
+++ web/frontend/src/components/Login.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import { sendRequest } from '../common/Requests';
+import { sendOutsideRequest } from '../common/Requests';
 
 function Login() {
-    const doLogin = async event => {
-        sendRequest('login', (res) => {
-            console.log(res);
+    const [username, setUsername] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
+    const doLogin = async (e) => {
+        e.preventDefault()
+
+        const URL = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC7OHvwvqRgrOvgYoy2C5sgnXSZ02xLZPc';
+        const payload = {
+            "email": username,
+            "password": password,
+            "returnSecureToken": true
+        }
+
+
+        sendOutsideRequest(URL, payload, (res) => {
+            const { localId } = JSON.parse(res.responseText);
+            console.log(localId);
+        }, (err) => {
+            console.log(err);
         });
     };
 
@@ -12,12 +28,20 @@ function Login() {
         <main className='flex h-minus-header'>
             <div className='grid container m-auto min-h-1/3 bg-yellow-200 place-items-center flex-1'
                 id='loginDiv'>
-                <form onSubmit={doLogin}>
-                    <span id='inner-title'>Please Log In</span><br />
-                    <input className='mt-5 px-2' type='text' id='loginName' placeholder='Username' /><br />
-                    <input className='mt-5 mb-5 px-2' type='password' id='loginPassword' placeholder='Password' /><br />
-                    <input type='submit' id='loginButton' className='w-40 bg-red-500' value='Log in'
-                        onClick={doLogin} />
+                <form className='grid grid-cols-1 gap-6' onSubmit={doLogin}>
+                    <div className='grid grid-cols-1 gap-2'>
+                        <label htmlFor='username'>Username</label>
+                        <input type='text' name='username' id='username' value={username}
+                            onChange={(e) => setUsername(e.target.value)} />
+                    </div>
+                    <div className='grid grid-cols-1 gap-2'>
+                        <label htmlFor='password'>Password</label>
+                        <input type='password' name='password' id='password' value={password}
+                            onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                    <div className='grid grid-cols-1 gap-2'>
+                        <button type='submit' className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Login</button>
+                    </div>
                 </form>
                 <span id='loginResult'></span>
             </div>

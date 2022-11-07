f9d9fe96c69539807f6556fa6d62037bfef80628
Sabrina-Lopez <32684344+Sabrina-Lopez@users.noreply.github.com>
2022-11-07 15:58:02 -0500

    edited index.js for functions

--- web/functions/index.js
+++ web/functions/index.js
@@ -1,15 +1,8 @@
 //import libraries
-<<<<<<< HEAD
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const express = require("express");
 const bodyParser = require("body-parser");
-=======
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
-import * as express from 'express';
-import * as bodyParser from "body-parser";
->>>>>>> a2612daa31d6922012b3c769eb516c580cf83b43
 
 //initialize firebase inorder to access its services
 admin.initializeApp(functions.config().firebase);
@@ -28,24 +21,14 @@ const db = admin.firestore();
 const userCollection = 'users';
 
 //define google cloud function name
-<<<<<<< HEAD
 exports.webApi = functions.https.onRequest(main);
-=======
-export const webApi = functions.https.onRequest(main);
->>>>>>> a2612daa31d6922012b3c769eb516c580cf83b43
 
 class User {
     constructor(userID, bills, budgets, categories) {
         this.userID = String,
-<<<<<<< HEAD
         this.bills = [],
         this.budgets = [],
         this.categories = [];
-=======
-            this.bills = [],
-            this.budgets = [],
-            this.categories = [];
->>>>>>> a2612daa31d6922012b3c769eb516c580cf83b43
     }
 }
 

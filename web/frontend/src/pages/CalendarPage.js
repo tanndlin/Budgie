79798cf3098e41dba16068b2c8c08827a9bc9725
Tanndlin <tanndlin@gmail.com>
2022-11-07 21:15:33 -0500

    Get user on login

--- web/frontend/src/pages/CalendarPage.js
+++ web/frontend/src/pages/CalendarPage.js
@@ -3,6 +3,21 @@ import BigCalendar from '../components/Calendar';
 import BudgetsView from '../components/BudgetsView';
 
 function CalendarPage(props) {
+    if (props.user === null) {
+        // Check url for localId
+        const url = new URL(window.location.href);
+        const localId = url.searchParams.get('user');
+        if (localId) {
+            // If localId is found, set user to localId
+            props.setUser(localId);
+
+            // TODO: Hydrate bills and budgets
+        } else {
+            // If localId is not found, redirect to login page
+            window.location.href = '/';
+        }
+    }
+
     // Does both operations because 2 setstates overwrite each other
     function modifyEvents(add, remove) {
         if (remove) {
--- web/frontend/src/pages/ListPage.js
+++ web/frontend/src/pages/ListPage.js
@@ -3,6 +3,21 @@ import BudgetsView from '../components/BudgetsView';
 import ListView from '../components/ListView';
 
 function ListPage(props) {
+    if (props.user === null) {
+        // Check url for localId
+        const url = new URL(window.location.href);
+        const localId = url.searchParams.get('user');
+        if (localId) {
+            // If localId is found, set user to localId
+            props.setUser(localId);
+
+            // TODO: Hydrate bills and budgets
+        } else {
+            // If localId is not found, redirect to login page
+            window.location.href = '/';
+        }
+    }
+
     function modifyEvents(add, remove) {
         if (remove) {
             props.setBills([...props.bills.filter((e) => e !== remove), add]);

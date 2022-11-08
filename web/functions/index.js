cc3a1eff742013d3dff441663b3f36a6d0f9f116
Melanie Ehrlich <melanie6104@icloud.com>
2022-11-08 00:52:43 -0500

    add create bill function

--- web/functions/index.js
+++ web/functions/index.js
@@ -19,6 +19,8 @@ main.use(bodyParser.urlencoded({ extended: false }));
 //initialize the database and the collection 
 const db = admin.firestore();
 const userCollection = 'users';
+const billCollection = 'bills';
+
 
 //define google cloud function name
 exports.webApi = functions.https.onRequest(main);
@@ -38,7 +40,7 @@ class Bill {
         this.billID = String,
         this.name = String,
         this.categoryID = String,
-        this.price = float,
+        this.price = Number,
         this.startDate = Date,
         this.endDate = Date,
         this.recurrence = String,
@@ -50,8 +52,8 @@ class Budget {
     constructor(budgetID, name, expectedPrice, actualPrice, startDate, endDate, recurrence, payments) {
         this.budgetID = String,
         this.name = String,
-        this.expectedPrice = float,
-        this.actualPrice = float,
+        this.expectedPrice = Number,
+        this.actualPrice = Number,
         this.startDate = Date,
         this.endDate = Date,
         this.recurrence = String,
@@ -76,9 +78,34 @@ app.post('/users', async (req, res) => {
             categories: []
         }
 
-        const newDoc = await db.collection(userCollection).add(user);
+        const newDoc = await db.collection(userCollection).add(User);
         res.status(201).send(`Created a new user: ${newDoc.id}`);
     } catch (error) {
         res.status(400).send(`User addition to database was unsuccessful`)
+    }
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+  })
+
+// Create bill
+app.post('/bills', async (req, res) => {
+    try {
+        const Bill = {
+            //billID: String,
+            name: String,
+            categoryID: String,
+            price: Number,
+            startDate: Date,
+            endDate: Date,
+            recurrence: String,
+            lastPaidDate: Date
+        }
+
+        const newDoc = await db.collection(billCollection).add(Bill);
+        res.status(201).send(`Created a new bill: ${newDoc.id}`);
+    } catch (error) {
+        res.status(400).send(`Bill addition to database was unsuccessful`)
     }
 });

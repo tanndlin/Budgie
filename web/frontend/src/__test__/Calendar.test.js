92d81e74d290dd986ae8391c0a238f45ff539a95
Tanner Sandlin <tanndlin@gmail.com>
2022-11-08 09:47:56 -0500

    Remove Testing for ListView

--- web/frontend/src/__test__/Calendar.test.js
+++ web/frontend/src/__test__/Calendar.test.js
@@ -30,3 +30,26 @@ test('Events green on paid', () => {
     const event = screen.getByText(/Hello World/);
     expect(event).toHaveStyle('background-color: rgb(74 222 128) !important');
 });
+
+test('Sums total of bills', () => {
+    const bills = [];
+    for (let i = 0; i < 10; i++) {
+        bills.push({
+            title: 'Hello World',
+            start: new Date(),
+            end: new Date(),
+            allDay: true,
+            resources: { paid: false, },
+            amount: Math.floor(Math.random() * 100),
+        });
+    }
+
+    render(<BigCalendar
+        bills={bills}
+    />);
+
+
+    const sum = bills.reduce((acc, bill) => acc + bill.amount, 0);
+    const event = screen.getByText(new RegExp(`${sum}`));
+    expect(event).toBeInTheDocument();
+});
--- web/frontend/src/__test__/Paid.test.js
+++ web/frontend/src/__test__/Paid.test.js
@@ -1,25 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import BigCalendar from '../components/Calendar';
-import ListView from '../components/ListView';
-
-test('Ctrl Clicking toggles paid on listView', () => {
-    render(<ListView
-        bills={[{
-            title: 'Hello World',
-            start: new Date(),
-            end: new Date(),
-            allDay: true,
-            resources: { paid: false, },
-            amount: 100,
-        }]}
-    />);
-
-
-    // Ctrl + Click on the bill
-    const event = screen.getByText('Hello World');
-    event.dispatchEvent(new MouseEvent('click', { ctrlKey: true }));
-    expect(event).toHaveStyle('background-color: rgb(74 222 128) !important');
-});
 
 test('Ctrl Clicking toggles paid on Calendar', () => {
     render(<BigCalendar
@@ -38,25 +18,6 @@ test('Ctrl Clicking toggles paid on Calendar', () => {
     const event = screen.getByText('Hello World');
     event.dispatchEvent(new MouseEvent('click', { ctrlKey: true }));
     expect(event).toHaveStyle('background-color: rgb(74 222 128) !important');
-});
-
-test('Ctrl Clicking toggles paid off listView', () => {
-    render(<ListView
-        bills={[{
-            title: 'Hello World',
-            start: new Date(),
-            end: new Date(),
-            allDay: true,
-            resources: { paid: true, },
-            amount: 100,
-        }]}
-    />);
-
-
-    // Ctrl + Click on the bill
-    const event = screen.getByText('Hello World');
-    event.dispatchEvent(new MouseEvent('click', { ctrlKey: true }));
-    expect(event).toHaveStyle('background-color: rgb(248 113 113) !important');
 });
 
 test('Ctrl Clicking toggles paid off Calendar', () => {

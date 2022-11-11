e289c1af7ee30212054fa6e5d5651ba074c01d11
Sabrina-Lopez <32684344+Sabrina-Lopez@users.noreply.github.com>
2022-11-11 01:23:27 -0500

    various changes (#1)

--- web/functions/index.js
+++ web/functions/index.js
@@ -3,20 +3,13 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require('cors');  
 
 //initialize firebase inorder to access its services
 admin.initializeApp(functions.config().firebase);
 
 //initialize express server
 const app = express();
-//const main = express();
-
-//add the path to receive request and set json as bodyParser to process the body 
-/*
-main.use('/api/v1', app);
-main.use(bodyParser.json());
-main.use(bodyParser.urlencoded({ extended: false }));
-*/
 
 //initialize the database and the collection 
 const db = admin.firestore();
@@ -24,35 +17,39 @@ const userCollection = 'users';
 const billCollection = 'bills';
 const budgetCollection = 'budgets';
 const categoryCollection = 'categories';
-const userProfileCollection = 'userprofiles';
-
 
 //define google cloud function name
 exports.webApi = functions.https.onRequest(app);
 
+app.use(
+    cors({
+        origin: "*",
+    })
+);
+
 //classes for database objects
 
 class Bill {
-    constructor(name, amount, startDate, endDate, recurrence, category, lastPaidDate) {
+    constructor(name, categoryId, color, price, startDate, endDate, recurrence, isPaid) {
         this.name = name,
-        this.amount = amount,
+        this.categoryId = categoryId,
+        this.color = color,
+        this.price = price,
         this.startDate = startDate,
         this.endDate = endDate,
         this.recurrence = recurrence,
-        this.category = category,
-        this.lastPaidDate = lastPaidDate
+        this.isPaid = isPaid
     }
 }
 
 class Budget {
-    constructor(name, budgetAmount, amountSpent, startDate, endDate, recurrence, category, payments) {
+    constructor(name, categoryId, expectedPrice, actualPrice, startDate, recurrence, payments) {
         this.name = name,
-        this.budgetAmount = budgetAmount,
-        this.amountSpent = amountSpent,
+        this.categoryId = categoryId,
+        this.expectedPrice = expectedPrice,
+        this.actualPrice = actualPrice,
         this.startDate = startDate,
-        this.endDate = endDate,
         this.recurrence = recurrence,
-        this.category = category,
         this.payments = payments
     }
 }
@@ -64,15 +61,28 @@ class Category {
 }
 
 class UserProfile {
-    constructor(firstName, lastName, email, expectedIncome) {
+    constructor(firstName, lastName, expectedIncome) {
         this.firstName = firstName,
         this.lastName = lastName,
-        this.email = email,
         this.expectedIncome = expectedIncome
     }
 }
 
+class OneOffs {
+    constructor(name, categoryId, color, price, date) {
+        this.name = name,
+        this.categoryId = categoryId,
+        this.color = color,
+        this.price = price,
+        this.date = date
+    }
+}
+
+//TO DO: check category stuff for correct syntax, test all current API, add API for one-offs
+
 app.post('/test', async (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+
     try {
 
         const word = req.body.word;
@@ -84,6 +94,8 @@ app.post('/test', async (req, res) => {
 });
 
 app.post('/CreateUser', async (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+
     try {
 
         const userId = req.body.userId;
@@ -95,71 +107,61 @@ app.post('/CreateUser', async (req, res) => {
     }
 });
 
-//TO DO: ENSURE THE BELOW API HAVE THE CORRECT SYNTAX TO PROPERLY ACCESS FIRESTORE
-
 app.post('/CreateUserProfile', async (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+
     try{
 
         const userId = req.body.userId; 
         const userRef = db.collection(userCollection).doc(`${userId}`);
 
-        //check if the user profile already exists
-        const profileExist = await userRef.collection(userProfileCollection).where('name', '==', req.body.userEmail).get();
-        if(!profileExist.empty){
-            res.status(400).send("This user profile already exists")
-        }
-
-        // constructor for a new user profile
+        //constructor for a new user profile
         const newProfile = new UserProfile(
             req.body.firstName,
             req.body.lastName,
-            req.body.userEmail,
             req.body.expectedIncome
         );
 
-        // add it to the collection of new user profiles
-        const newProfileDoc = await userRef.collection(userProfileCollection).add(newProfile);
+        //add the user's profile to the database
+        await userRef.set(newProfile);
 
-        res.status(201).send(`User profile creation for ${userId} to database was successful, "newProfileId": ${newProfileDoc.id}}`);
+        res.status(201).send(`User profile creation for ${userId} to database was successful`);
 
     } catch (error) {
-        res.status(400).send(`User profile creation to database was unsuccessful`);
+        res.status(400).send(`${error.message}`);
     }
 });
 
 app.post('/EditUser', async (req, res) => {
-    try{
+    res.set('Access-Control-Allow-Origin', '*');
 
-        // if the user profile does not exist, we cannot edit it
-        const profileExist = await userRef.collection(userProfileCollection).where('name', '==', req.body.userEmail).get();
-        if(profileExist.empty){
-            res.status(400).send("This user profile does not exist")
-        }
+    try{
 
         const userId = req.body.userId; 
         const userRef = db.collection(userCollection).doc(`${userId}`);
         const profileId = req.body.profileId; 
 
-        // edit the user profile fields
+        //edit the user profile fields
         const editedProfile = new UserProfile(
             req.body.firstName,
             req.body.lastName,
-            req.body.userEmail,
             req.body.expectedIncome
         );
 
-        // add the updated user profile to the database
-        await userRef.collection(userProfileCollection).doc(profileId).update(editedProfile);
+        //update the user profile to the database
+        await userRef.update(editedProfile);
 
         res.status(200).send(`User profile update for ${userId} to database was successful`);
 
     } catch (error) {
-        res.status(400).send(`User profile update to database was unsuccessful`);
+        res.status(400).send(`${error.message}`);
     }
 });
 
 //create bill
 app.post('/CreateBill', async (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+
     try {
 
         const userId = req.body.userId; 
@@ -192,26 +194,28 @@ app.post('/CreateBill', async (req, res) => {
 
         // constructor for a new bill
         const newBill = new Bill(
-            req.body.billName,
-            req.body.billAmount,
-            req.body.billStartDate,
-            req.body.billEndDate,
-            req.body.billRecurrence,
-            categoryId,
-            req.body.lastPaidDate
+            req.body.name,
+            req.body.categoryId,
+            req.body.color,
+            req.body.price,
+            req.body.startDate,
+            req.body.endDate,
+            req.body.recurrence,
+            req.body.isPaid //need to have the array or collection created and later maintained
         );
 
-        const newBillDoc = await userRef.collection(billCollection).add(newBill);
+        const newBillDoc = await userRef.collection(billCollection).doc().set(newBill);
         res.status(201).send(`{"userId": ${userId}, "newBillId": ${newBillDoc.id}}`);
 
     } catch (error) {
-        res.status(400).send(`Bill addition to database was unsuccessful`)
+        res.status(400).send(`${error.message}`)
     }
 });
 
-
 //edit bill
 app.post('/EditBill', async (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+
     try {
 
         const userId = req.body.userId; 
@@ -232,34 +236,37 @@ app.post('/EditBill', async (req, res) => {
         categoryId = categoryDoc.id;
 
         const editedBill = new Bill(
-            req.body.billName,
-            req.body.billAmount,
-            req.body.billStartDate,
-            req.body.billEndDate,
-            req.body.billRecurrence,
-            categoryId,
-            req.body.lastPaidDate
+            req.body.name,
+            req.body.categoryId,
+            req.body.color,
+            req.body.price,
+            req.body.startDate,
+            req.body.endDate,
+            req.body.recurrence,
+            req.body.isPaid
         );
 
-        await userRef.collection(billCollection).doc(billId).update(editedBill);
+        await userRef.collection(billCollection).doc(`${billId}`).update(editedBill);
         res.status(200).send(`{"userId": ${userId}, "editedBillId": ${billId}}`);
 
     } catch (error) {
-        res.status(400).send(`Bill update to database was unsuccessful`)
+        res.status(400).send(`${error.message}`)
     }
 });
 
 //remove bill
 app.post('/RemoveBill', async (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+
     try {
 
         const userId = req.body.userId; 
         const userRef = db.collection(userCollection).doc(`${userId}`);
         const billId = req.body.billId;
-        const billDoc =  await userRef.collection(billCollection).doc(billId).get();
+        const billDoc =  await userRef.collection(billCollection).doc(`${billId}`).get();
 
         if(billDoc.exists) {
-            await userRef.collection(billCollection).doc(billId).delete();
+            await userRef.collection(billCollection).doc(`${billId}`).delete();
             res.status(200).send(`{"userId": ${userId}, "billId": "NULL"}`);
         }
         else {
@@ -267,12 +274,14 @@ app.post('/RemoveBill', async (req, res) => {
         }
 
     } catch (error) {
-        res.status(400).send(`Bill removal to database was unsuccessful`)
+        res.status(400).send(`${error.message}`)
     }
 });
 
 //create budget
 app.post('/CreateBudget', async (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+
     try {
 
         const userId = req.body.userId; 
@@ -299,27 +308,27 @@ app.post('/CreateBudget', async (req, res) => {
         const budgetRespectiveBills = await userRef.collection(billCollection).where('category', '==', category).get();
 
         const newBudget = new Budget(
-            req.body.budgetName,
-            req.body.budgetAmount,
-            req.body.amountSpent,
+            req.body.name,
+            req.body.categoryId,
+            req.body.expectedPrice,
+            req.body.actualPrice,
             req.body.startDate,
-            req.body.endDate,
-            req.body.budgetRecurrence,
-            categoryId,
-            budgetRespectiveBills
+            req.body.recurrence,
+            req.body.payments //need to have the array or collection created and later maintained
         );
 
-        const newBudgetDoc = await userRef.collection(budgetCollection).add(newBudget);
+        const newBudgetDoc = await userRef.collection(budgetCollection).doc().set(newBudget);
         res.status(201).send(`{"userId": ${userId}, "newBudgetId": ${newBudgetDoc.id}, "budgetRespectiveBills": ${budgetRespectiveBills}}`);
 
     } catch (error) {
-        res.status(400).send(`Budget addition to database was unsuccessful`)
+        res.status(400).send(`${error.message}`)
     }
 });
 
-
 //edit budget
 app.post('/EditBudget', async (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+
     try {
 
         const userId = req.body.userId; 
@@ -341,35 +350,36 @@ app.post('/EditBudget', async (req, res) => {
         const budgetRespectiveBills = await userRef.collection(billCollection).where('category', '==', category).get();
 
         const editedBudget = new Budget(
-            req.body.budgetName,
-            req.body.budgetAmount,
-            req.body.amountSpent,
+            req.body.name,
+            req.body.categoryId,
+            req.body.expectedPrice,
+            req.body.actualPrice,
             req.body.startDate,
-            req.body.endDate,
-            req.body.budgetRecurrence,
-            categoryId,
-            budgetRespectiveBills
+            req.body.recurrence,
+            req.body.payments
         );
 
-        await userRef.collection(budgetCollection).doc(budgetId).update(editedBudget);
+        await userRef.collection(budgetCollection).doc(`${budgetId}`).update(editedBudget);
         res.status(200).send(`{"userId": ${userId}, "editedBudgetId": ${budgetId}, "budgetRespectiveBills": ${budgetRespectiveBills}}`);
 
     } catch (error) {
-        res.status(400).send(`Budget update to database was unsuccessful`)
+        res.status(400).send(`${error.message}`)
     }
 });
 
 //remove budget
 app.post('/RemoveBudget', async (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+
     try {
 
         const userId = req.body.userId; 
         const userRef = db.collection(userCollection).doc(`${userId}`);
         const budgetId = req.body.budgetId; 
-        const budgetDoc = await userRef.collection(budgetCollection).doc(budgetId).get();
+        const budgetDoc = await userRef.collection(budgetCollection).doc(`${budgetId}`).get();
 
         if(budgetDoc.exists) {
-            await userRef.collection(budgetCollection).doc(budgetId).delete();
+            await userRef.collection(budgetCollection).doc(`${budgetId}`).delete();
             res.status(200).send(`{"userId": ${userId}, "budgetId": "NULL"}`);
         }
         else {
@@ -377,7 +387,7 @@ app.post('/RemoveBudget', async (req, res) => {
         }
 
     } catch (error) {
-        res.status(400).send(`Budget removal to database was unsuccessful`)
+        res.status(400).send(`${error.message}`)
     }
 });
 

321943864d25babca9e89382c8af62bbb8ea6178
Tanndlin <tanndlin@gmail.com>
2022-11-14 19:57:37 -0500

    Fix test

--- web/frontend/src/__test__/Category.test.js
+++ web/frontend/src/__test__/Category.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import BigCalendar from '../components/Calendar';
+import { BigCalendar } from '../components/Calendar';
 
 test('Shows all unfiltered', () => {
     const categories = [

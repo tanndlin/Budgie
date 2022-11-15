83d7af7782ef2f147778ee4589b6a01e37cf9854
Tanndlin <tanndlin@gmail.com>
2022-11-15 12:44:38 -0500

    Remove save button

--- web/frontend/src/components/Header.js
+++ web/frontend/src/components/Header.js
@@ -1,41 +1,8 @@
 import { React } from 'react';
 import { Link } from 'react-router-dom';
-import { sendRequest } from '../common/Requests';
 import logo from '../img/logo.png';
 
 function Header(props) {
-    // TODO: Implement this once API is done
-    function save() {
-        props.bills.forEach((bill) => {
-            const isNew = !bill.id;
-            if (isNew) {
-                sendRequest(
-                    'CreateBill',
-                    bill,
-                    (res) => {
-                        console.log('Saved new bill', res);
-                        const resBill = JSON.parse(res.responseText);
-                        bill.id = resBill.id;
-                    },
-                    (err) => {
-                        console.log('Error creating bill', err);
-                    }
-                );
-            } else {
-                sendRequest(
-                    'EditBill',
-                    bill,
-                    (res) => {
-                        console.log('Saved bill', res);
-                    },
-                    (err) => {
-                        console.log('Error updating bill', err);
-                    }
-                );
-            }
-        });
-    }
-
     return (
         <header className="bg-[#BBE9E7] h-16 grid grid-cols-3 sticky top-0 z-50">
             <img className="h-16" src={logo} alt="Bird Logo" />
@@ -45,12 +12,6 @@ function Header(props) {
 
             {props.showNav && (
                 <span className="flex flex-cols gap-x-3 m-auto">
-                    <input
-                        className="font-bold hover:text-[#189DFD]"
-                        type="button"
-                        value="Save"
-                        onClick={save}
-                    />
                     <Link
                         className="font-bold hover:text-[#189DFD]"
                         to={{

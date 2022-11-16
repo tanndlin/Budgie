1339fb1a41b1a1cdfe646f03558e28e6b893dca8
Tanner Sandlin <tanner.sandlin@gtri.gatech.edu>
2022-11-16 11:40:56 -0500

    Fix BillIsCurrentLogic

--- web/frontend/src/components/ExtraneousView.js
+++ web/frontend/src/components/ExtraneousView.js
@@ -3,7 +3,6 @@ import { sendRequest } from '../common/Requests';
 import Extra from './Extra';
 
 function ExtraneousView(props) {
-    console.log(props.extras);
     function newExtraneous() {
         const extra = {
             name: 'New One Off',

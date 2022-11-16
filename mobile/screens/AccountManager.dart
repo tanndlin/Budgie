91855d67b7562b7fe1ab54dc4c2c2482d3dd80e7
Alan Nguyen <alann21102@gmail.com>
2022-11-16 14:45:31 -0500

    account page progress

--- mobile/screens/AccountManager.dart
+++ mobile/screens/AccountManager.dart
@@ -10,8 +10,13 @@ class AccountManager extends StatefulWidget {
 
 class _AccountManagerState extends State<AccountManager> {
   int selectedIndex = 4;
-  List<String> routes = ['/MainPage', '/Budget', '/BillPage', '/CalendarView', '/AccountManager'];
-
+  List<String> routes = [
+    '/MainPage',
+    '/Budget',
+    '/BillPage',
+    '/CalendarView',
+    '/AccountManager'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +32,7 @@ class _AccountManagerState extends State<AccountManager> {
     }
 
     return Container(
-      // BACKGROUND AND APPBAR
+        // BACKGROUND AND APPBAR
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topLeft,
@@ -40,13 +45,16 @@ class _AccountManagerState extends State<AccountManager> {
             ],
           ),
         ),
-        child: Scaffold (
+        child: Scaffold(
           backgroundColor: Colors.transparent,
           appBar: AppBar(
             centerTitle: false,
             title: const Text(
               'Account Details',
-              style: TextStyle(fontSize: 35, color: Color(0xFF2D4B03), fontWeight: FontWeight.bold),
+              style: TextStyle(
+                  fontSize: 35,
+                  color: Color(0xFF2D4B03),
+                  fontWeight: FontWeight.bold),
             ),
             actions: <Widget>[
               Padding(
@@ -56,8 +64,12 @@ class _AccountManagerState extends State<AccountManager> {
                     Navigator.pushNamed(context, '/MainPage');
                   },
                   // child: Icon(Icons.logout, color: Color(0xFF2D4B03), size: 35.0,),
-                  child: Image.asset('assets/images/budgie.png', scale: 0.1,),
-                ),)
+                  child: Image.asset(
+                    'assets/images/budgie.png',
+                    scale: 0.1,
+                  ),
+                ),
+              )
             ],
             automaticallyImplyLeading: false,
             flexibleSpace: Container(
@@ -74,6 +86,123 @@ class _AccountManagerState extends State<AccountManager> {
               ),
             ),
           ),
+
+          body: SingleChildScrollView(
+              child: Column(
+                  children: [
+                    const SizedBox(height: 10.0,),
+                    Padding(
+                        padding: EdgeInsets.only(left: 5.0, right: 5.0),
+                        child: Column(
+                          children: <Widget> [
+                            Container(
+                              width: MediaQuery.of(context).size.width,
+                              height: 380,
+                              decoration: BoxDecoration(
+                              color: Color(0xddb3e5fc),
+                              borderRadius: BorderRadius.circular(8),
+                              boxShadow: [ BoxShadow(
+                                  blurRadius: 8,
+                                  offset: Offset(0, 15),
+                                  color: Color(0xffe3e9e7).withOpacity(.5),
+                                  spreadRadius: -9)]
+                              ),
+
+                              child: Column(
+                                children: <Widget> [
+                                  Container(
+                                    child: Image.asset('assets/images/profile_icon.png'),
+                                  ),
+
+                                  Container(
+                                    child: const Text("Full Name")
+                                  ),
+
+                                  Container(
+                                      child: const Text("Email")
+                                  )
+                                ]
+                              )
+                            ),
+
+                            SizedBox(height: 10),
+
+                            SizedBox(
+                              width : MediaQuery.of(context).size.width,
+                              height: 70,
+                              child: TextButton(
+                                  child: const Text(
+                                      "Edit Profile",
+                                      style: TextStyle(fontSize: 20)
+                                  ),
+                                  style: ButtonStyle(
+                                      padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(15)),
+                                      foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                                      backgroundColor: MaterialStateProperty.all<Color>(Color(0xddb3e5fc)),
+                                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                          RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(8.0),
+                                          )
+                                      )
+                                  ),
+                                  onPressed: () => null
+                              ),
+                              ),
+
+                            SizedBox(height: 10),
+
+                            SizedBox(
+                              width : MediaQuery.of(context).size.width,
+                              height: 70,
+                              child: TextButton(
+                                  child: const Text(
+                                      "Reset Password",
+                                      style: TextStyle(fontSize: 20)
+                                  ),
+                                  style: ButtonStyle(
+                                      padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(15)),
+                                      foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                                      backgroundColor: MaterialStateProperty.all<Color>(Color(0xddb3e5fc)),
+                                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                          RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(8.0),
+                                          )
+                                      )
+                                  ),
+                                  onPressed: () => null
+                              ),
+                            ),
+
+                            SizedBox(height: 10),
+
+                            SizedBox(
+                              width : MediaQuery.of(context).size.width,
+                              height: 70,
+                              child: TextButton(
+                                  child: const Text(
+                                      "Logout",
+                                      style: TextStyle(fontSize: 20)
+                                  ),
+                                  style: ButtonStyle(
+                                      padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(15)),
+                                      foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                                      backgroundColor: MaterialStateProperty.all<Color>(Color(0xddb3e5fc)),
+                                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                          RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(8.0),
+                                          )
+                                      )
+                                  ),
+                                  onPressed: () => null
+                              ),
+                            )
+
+                          ]
+                        )
+                    ),
+                  ]
+              )
+          ),
           // BOTTOM NAV
           bottomNavigationBar: BottomNavigationBar(
             elevation: 0,
@@ -83,16 +212,25 @@ class _AccountManagerState extends State<AccountManager> {
             currentIndex: selectedIndex,
             onTap: onTabTapped,
             items: const [
-              BottomNavigationBarItem(icon: Icon(Icons.home, size: 35.0,), label: 'Home'),
-              BottomNavigationBarItem(icon: Icon(Icons.addchart, size: 35.0), label: 'Budget'),
-              BottomNavigationBarItem(icon: Icon(Icons.add_circle_outline, size: 35.0), label: 'Bill'),
-              BottomNavigationBarItem(icon: Icon(Icons.calendar_month, size: 35.0), label: 'Calendar'),
-              BottomNavigationBarItem(icon: Icon(Icons.account_circle, size: 35.0), label: 'Account'),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.home,
+                    size: 35.0,
+                  ),
+                  label: 'Home'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.addchart, size: 35.0), label: 'Budget'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.add_circle_outline, size: 35.0),
+                  label: 'Bill'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.calendar_month, size: 35.0),
+                  label: 'Calendar'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.account_circle, size: 35.0),
+                  label: 'Account'),
             ],
           ),
-        )
-    );
+        ));
   }
-
-
 }

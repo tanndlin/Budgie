8bc1ec0c45b31b03e2132dc4fa0cddac1a49a9f9
JenniferDaks <111821883+JenniferDaks@users.noreply.github.com>
2022-11-16 14:11:53 -0500

    add text to log in/sign up divider

--- web/frontend/src/components/Divider.js
+++ web/frontend/src/components/Divider.js
@@ -10,13 +10,27 @@ function Divider(props) {
     const leftClassName = arrowClassName + '-rotate-90 left';
     const rightClassName = arrowClassName + 'rotate-90 right';
 
+    const dividerText = 'text-center text-[#3B3548] text-6xl';
+
+    const textInvisible = dividerText + ' invisible';
+    const textVisible = dividerText + ' visible';
+
     return (
         <div id='divider' className={"duration-300 w-1/2 min-h-[348px] absolute right-0 group transform h-full bg-gradient-to-br from-[#479CE0] via-[#9ECFA2] to-[#E7E233]" + (props.dividerToggle ? '' : dividerPositionClass)} onClick={() => { props.setDividerToggle(!props.dividerToggle) }}>            
             <img className='absolute rounded-md h-full w-full opacity-[0.7]' src={DividerImage} alt='Wood Texture' />
+            <div className='relative mx-32 mb-5'>
+                <div className='absolute inset-x-0 top-5'>
+                    <p id='dividerText' className={props.dividerToggle ? textVisible : textInvisible}>Sign Up</p>
+                </div>
+                <div className='absolute inset-x-0 top-5'>
+                    <p id='dividerText' className={!props.dividerToggle ? textVisible : textInvisible}>Log In</p>
+                </div>  
+            </div>
+             
             <img id='bwLogo' className='absolute h-full w-full' src={bwLogo} alt='Bird Logo' />
             <svg className={!props.dividerToggle ? rightClassName : leftClassName} xmlns="http://www.w3.org/2000/svg" width="72" height="72" version="1.0" viewBox="0 0 54 54">
                 <path d="M12.7 13.5C.9 23.2-.8 25.8 2.2 28.8c2.4 2.4 5.5 1 14.3-6.3 5-4.1 9.6-7.5 10.4-7.5.7 0 5 3.1 9.5 6.9 9.9 8.4 12 9.5 14.5 8.2 2.2-1.2 3.3-3.5 2.5-5.5-.3-.8-5.5-5.5-11.5-10.6-15.2-12.7-14.6-12.6-29.2-.5z" /><path d="M12.7 32.5C.9 42.2-.8 44.8 2.2 47.8c2.4 2.4 5.5 1 14.3-6.3 5-4.1 9.6-7.5 10.4-7.5.7 0 5 3.1 9.5 6.9 9.9 8.4 12 9.5 14.5 8.2 2.2-1.2 3.3-3.5 2.5-5.5-.3-.8-5.5-5.5-11.5-10.6-15.2-12.7-14.6-12.6-29.2-.5z" />
-            </svg>       
+            </svg>      
         </div>
     );
 };

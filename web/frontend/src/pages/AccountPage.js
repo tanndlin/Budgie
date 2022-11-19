86de886fef6baa0d29d048c3a0fb49f8d51494b0
Tanner Sandlin <tanndlin@gmail.com>
2022-11-19 16:15:37 -0500

    Add Ability to Create Edit and Delete Categories (#39)

--- web/frontend/src/pages/AccountPage.js
+++ web/frontend/src/pages/AccountPage.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import Overview from '../components/Overview';
 import Account from '../components/Account';
+import CategoryView from '../components/CategoryView';
 
 function AccountPage(props) {
     return (
-        <main className="h-minus-header flex lg:flex-row sm:flex-col">
-            <div className="m-auto p-8 flex container max-w-[960px] bg-[#BBE9E7] bg-opacity-50 rounded-md">
+        <main className="h-minus-header flex flex-col gap-10">
+            <article className="mt-10 mx-auto p-8 flex container max-w-[960px] bg-[#BBE9E7] bg-opacity-50 rounded-md">
                 <div className="w-full py-4 h-full m-auto bg-[#b2c6ec] bg-opacity-[.7] rounded-md shadow-lg">
-                    <h1 className="text-center text-[#3B3548] text-6xl mb-16 mx-4 border-b-2 pb-4 border-[#3B3548]">
-                        My Account
-                    </h1>
-                    <article className="grid grid-cols-2">
+                    <header className="grid grid-cols-3 font-bold mb-3 border-black border-b-2 p-1">
+                        <h1 className="text-2xl">My Account</h1>
+                    </header>
+                    <section className="grid grid-cols-2">
                         <Overview {...props} />
                         <aside className="border-black border-l-2">
                             <Account {...props} />
                         </aside>
-                    </article>
+                    </section>
                 </div>
-            </div>
+            </article>
+
+            <article className="mb-5 mx-auto p-8 flex container max-w-[960px] bg-[#BBE9E7] bg-opacity-50 rounded-md">
+                <CategoryView {...props} />
+            </article>
         </main>
     );
 }

d8ce3e7144cd8929e9b02c437e52ba822541097a
tanndlin <tanndlin@gmail.com>
2022-11-21 09:38:28 -0600

    Hide Notif Provider if no notifs

--- web/frontend/src/components/NotificationProvider.js
+++ web/frontend/src/components/NotificationProvider.js
@@ -3,7 +3,7 @@ import Notification from './Notification';
 
 function NotificationProvider(props) {
     return (
-        <nav className="absolute top-[5rem] left-[1rem] grid grid-flow-row gap-4">
+        <nav className="absolute top-[5rem] left-[1rem] grid grid-flow-row gap-4 notificationProvider">
             {props.notifications &&
                 props.notifications.map((notif) => (
                     <Notification

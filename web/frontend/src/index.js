a9db322193740a58b5ea7dbc47cf0556576913fd
tanndlin <tanndlin@gmail.com>
2022-11-24 15:21:23 -0600

    trigger

--- web/frontend/src/index.js
+++ web/frontend/src/index.js
@@ -6,5 +6,3 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
-
-// If you want to start measuring performance in your app, pass a function

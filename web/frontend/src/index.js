b7077ba83a8016e9e23228c6a27fa856a8541dd2
tanndlin <tanndlin@gmail.com>
2022-11-24 15:20:10 -0600

    fuck

--- web/frontend/src/index.js
+++ web/frontend/src/index.js
@@ -8,4 +8,3 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))

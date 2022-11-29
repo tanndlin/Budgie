cceb10781f5291ffadf07e1dabb47bf1b32b4a80
JenniferDaks <111821883+JenniferDaks@users.noreply.github.com>
2022-11-28 22:55:40 -0500

    fix margins

--- web/frontend/src/pages/LoginPage.js
+++ web/frontend/src/pages/LoginPage.js
@@ -31,7 +31,7 @@ const LoginPage = (props) => {
 
     return (
         <article className="min-h-minus-header h-1 flex flex-col">
-            <main className="flex flex-col h-full">
+            <main className="flex flex-col h-full my-5">
                 <section className="grid grid-cols-2 w-3/4 max-w-[960px] m-auto relative min-w-[500px] h-3/4 max-h-[615px]">
                     <Login
                         email={loginEmail}
